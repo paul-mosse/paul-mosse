8af23884523fcc92fa7e262914dd6d1e2c7cc2c7
Bennett Gelly <bennettgelly@Bennetts-MacBook-Pro.local>
2019-07-14 22:52:47 -0400

    website test files

---
+++ ndxz-studio/site/js/paulmosse.js
@@ -0,0 +1,9 @@
+// Created 10th Feb for Theme Paul Mosse
+$(document).ready(function(){
+
+	// Replace the paragraph in #image_details with the Image Details DIV
+	var imgDetails = $('div.img_details');
+  $('#image_details').append( imgDetails );
+	
+});
+
